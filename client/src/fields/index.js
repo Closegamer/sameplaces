31d22e5c4ef51d1286955822976b150184808429
agrigoriev <agrigoriev@avinfors.ru>
2019-08-23 19:17:39 +0300

    DatePicker added

--- client/src/fields/index.js
+++ client/src/fields/index.js
@@ -3,5 +3,13 @@ import PassField from './PassField';
 import CheckBoxField from './CheckBoxField';
 import SelectField from './SelectField';
 import FileField from './FileField';
+import DatePickerField from './DatePickerField';
 
-export { TextField, PassField, CheckBoxField, SelectField, FileField };
+export {
+  DatePickerField,
+  TextField,
+  PassField,
+  CheckBoxField,
+  SelectField,
+  FileField
+};
